9ef5699dd9ac4dabb377719689bbf8dde1cc8343
ne1252 <turanskiy.p@gmail.com>
2024-11-05 22:44:14 +0200

    Initial commit

---
+++ js1.js
@@ -0,0 +1,21 @@
+let x1 = 'hello'
+let x2 = 'owu'
+let x3 = 'ua'
+let x4 = 1
+let x5 = 10
+let x6 = -999
+let x7 = -999
+let x8 = 123
+let x9 = 3.14
+let x10 = 2.7
+let x11 = 16
+let x12 = true
+let x13 = false
+console.log(x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13)
+
+
+
+
+
+
+
