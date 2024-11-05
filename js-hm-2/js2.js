366848e3a8820006b5bdcd4bc061de266530d808
ne1252 <turanskiy.p@gmail.com>
2024-11-05 22:51:01 +0200

    1

---
+++ js-hm-2/js2.js
@@ -0,0 +1,6 @@
+/*Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person ' +
+'(Не об'єкт, просто за допомоги конкатенації)*/
+let firstName = 'Павло'
+let middleName = 'Іванович'
+let lastName = 'Туранський'
+console.log( lastName, firstName, middleName)
